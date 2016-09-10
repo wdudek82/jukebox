c1dee19fab5019061ec1d4477616d64d4b5a96ee
Wojciech Dudek <wdudek82@gmail.com>
2016-09-10 12:17:37 +0200

    Removed pager from api

--- jukebox/settings.py
+++ jukebox/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10,
+    # 'PAGE_SIZE': 10,
 }
 
 
